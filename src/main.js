0f9f25d0009519e811c6f09dd355dbf0c5e68ab0
y_sugiyama_gxp <y.sugiyama@gxp.co.jp>
2022-11-14 19:23:06 +0900

    implement edit todo text

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let input;
-let todoList;
-let target;
+let form, input, todoList;
+
 var append = function (htmlStr, parentSelector) {
   var tmpElem;
 
@@ -15,19 +14,23 @@ var append = function (htmlStr, parentSelector) {
 
   return document.querySelector(parentSelector).appendChild(tmpElem.firstChild);
 };
+
 async function add() {
   // todoList.textContent = await invoke("greet", { name: input.value });
   if(input.value == ""){
     return
   }
-  const added = append(`<div class=todo><input class=todoInput value=${input.value}></input><button class=todoButton>Done</button></div>`, '#todoList')
-  added.childNodes[0].addEventListener("click", (e) => edit(e.target));
+  const added = append(`<div title=${input.value} class=todo><form class=todoForm><input class=todoInput value=${input.value}></input></form><button class=todoButton>Done</button></div>`, '#todoList')
+  added.childNodes[0].addEventListener("submit", (e) => {e.preventDefault(); e.target.childNodes[0].blur()});
+  added.childNodes[0].childNodes[0].addEventListener("input", (e) => edit(e.target));
   added.childNodes[1].addEventListener("click", (e) => done(e));
   input.value = ""
+  input.focus();
 }
 
 async function edit(target) {
   // todoList.textContent = await invoke("greet", { name: input.value });
+  console.log(target.value);
 }
 
 async function done(e) {
@@ -36,9 +39,12 @@ async function done(e) {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+  form = document.querySelector("#form");
   input = document.querySelector("#input");
   todoList = document.querySelector("#todoList");
   document
     .querySelector("#button")
     .addEventListener("click", () => add());
+
+  form.addEventListener("submit", (e) => {e.preventDefault(); add()});
 });

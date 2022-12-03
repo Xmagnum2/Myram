03f593069c0da0ba2c0915cc71a938543a25a8aa
Xmagnum2 <pikatyuu5p@gmail.com>
2022-12-04 06:40:22 +0900

    アプリ名をMyramに変更

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 const { BaseDirectory, readTextFile, writeTextFile } = window.__TAURI__.fs;
 
 let form, input, todoList;
-var append = function (htmlStr, parentSelector) {
+var append = (htmlStr, parentSelector) => {
   var tmpElem;
 
-  if (typeof htmlStr !== 'string' ||
-    typeof parentSelector !== 'string' ||
-    document.querySelector(parentSelector).length === 0) return;
+  if (typeof htmlStr !== "string" || typeof parentSelector !== "string" || document.querySelector(parentSelector).length === 0) return;
 
-  tmpElem = document.createElement('div');
+  tmpElem = document.createElement("div");
   tmpElem.innerHTML = htmlStr;
 
   return document.querySelector(parentSelector).appendChild(tmpElem.firstChild);
@@ -16,44 +14,52 @@ var append = function (htmlStr, parentSelector) {
 
 async function add(value) {
   if (input.value === "" && value === undefined) {
-    return
+    return;
   }
   if (value === undefined) value = input.value;
-  const added = append(`<div title="${value}" class=todo><form class=todoForm><input class=todoInput value="${value}"></input></form><button class=todoButton>Done</button></div>`, '#todoList')
-  added.childNodes[0].addEventListener("submit", (e) => { e.preventDefault(); e.target.childNodes[0].blur() });
+  const added = append(`<div title="${value}" class=todo><form class=todoForm><input class=todoInput value="${value}"></input></form><button class=todoButton>Done</button></div>`, "#todoList");
+  added.childNodes[0].addEventListener("submit", (e) => {
+    e.preventDefault();
+    e.target.childNodes[0].blur();
+  });
   added.childNodes[0].childNodes[0].addEventListener("input", edit);
   added.childNodes[1].addEventListener("click", done);
-  input.value = ""
+  input.value = "";
   input.focus();
-  save();
+  await save();
 }
 
 async function edit(e) {
-  e.path[2].title = e.target.value;
-  save();
+  e.composedPath()[2].title = e.target.value;
+  await save();
 }
 
 async function done(e) {
-  e.path[1].remove();
-  save();
+  e.composedPath()[1].remove();
+  await save();
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
   form = document.querySelector("#form");
   input = document.querySelector("#input");
   todoList = document.querySelector("#todoList");
-  setup();
+  await setup();
 
-  form.addEventListener("submit", (e) => { e.preventDefault(); add() });
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    await add();
+  });
 });
 
 async function setup() {
   try {
     const content = await readTextFile("storage.txt", {
-      dir: BaseDirectory.AppLocalData
-    })
-    content.split(" ").map(e => decodeURI(e)).forEach(add);
-    console.log(content.split(" ").map(e => {console.log(decodeURI(e)); return decodeURI(e)}));
+      dir: BaseDirectory.Resource,
+    });
+    content
+      .split(" ")
+      .map((e) => decodeURI(e))
+      .forEach(add);
   } catch (e) {
     console.log(e);
   }
@@ -61,9 +67,15 @@ async function setup() {
 
 async function save() {
   try {
-    await writeTextFile("storage.txt", Array.from(todoList.children).map(e => encodeURI(e.title)).join(" "), {
-      dir: BaseDirectory.AppLocalData
-    });
+    await writeTextFile(
+      "storage.txt",
+      Array.from(todoList.children)
+        .map((e) => encodeURI(e.title))
+        .join(" "),
+      {
+        dir: BaseDirectory.Resource,
+      }
+    );
   } catch (e) {
     console.log(e);
   }

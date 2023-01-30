001618d82631a8d268dd37e1f84a566f934b14fc
y_sugiyama_gxp <y.sugiyama@gxp.co.jp>
2023-01-30 13:57:35 +0900

    ウィンドウのアイコンの色を調整

--- src/js/window.js
+++ src/js/window.js
@@ -18,7 +18,7 @@ function blackOrWhite(hexcolor) {
   var r = parseInt(hexcolor.substr(1, 2), 16);
   var g = parseInt(hexcolor.substr(3, 2), 16);
   var b = parseInt(hexcolor.substr(5, 2), 16);
-  return ((((r * 299) + (g * 587) + (b * 114)) / 1000) < 128) ? "white" : "black";
+  return ((((r * 299) + (g * 587) + (b * 114)) / 1000) < 128) ? "rgb(180, 180, 180)" : "black";
 }
 
 function rgba2hex(orig) {
